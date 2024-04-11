ea090a57ce1e85707876f1a47529611c534ca144
rayalva407 <rayalva407@gmail.com>
2024-04-11 12:33:30 -0600

    feat: allows for the creation of a code for the game

--- mastermind.rb
+++ mastermind.rb
@@ -1,3 +1,18 @@
 class Mastermind
+  def initialize
+    @code = []
+    @guesses = []
+    @feedback = []
+  end
 
+  def create_code
+    puts "Choose a code of four colors separated by spaces from the following: red, green, blue, yellow, orange, purple"
+    puts "Example: 'red green blue yellow' is a valid code"
+
+    @code.append(gets.chomp.split(" "))
+  end
 end
+
+game = Mastermind.new
+
+game.choose_code
